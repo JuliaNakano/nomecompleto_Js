84f4aba2d43c5cc0ffb859d38ae8813fc70bd6a8
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-05-26 20:07:07 -0300

    Atividade nome completo

---
+++ batata.js
@@ -0,0 +1,8 @@
+let nome = prompt("Digite seu primeiro nome:..");
+
+let sobrenome = prompt ("Digite seu sobrenome:");
+
+
+alert(`Nome completo: ${nome} ${sobrenome}`);
+
+alert(`Nome de catálogo: ${sobrenome.toUpperCase()}, ${nome} `);
